a615cc731da9e426f9a40569b49565d91de6953a
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-13 16:25:52 +0100

    Created test_login function

--- authentication/tests.py
+++ authentication/tests.py
@@ -8,3 +8,12 @@ class AuthenticationTest(TestCase):
         self.client = Client()
         self.login_url = reverse('login')
         self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_login(self):
+        url = reverse('login')
+        response = self.client.post(url, {
+            'username': 'testuser',
+            'password': 'testpass'
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('home'))
