eb236dd7d1fa6dee6ca981b1da709774634db875
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-13 16:17:15 +0100

    Created Login Test Class

--- authentication/tests.py
+++ authentication/tests.py
@@ -2,4 +2,9 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-# Create your tests here.
+# Created Login Test
+class LoginTraderTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse('login')
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
