8fe01a0649f6325e6917758e3d482c306522b940
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-13 18:47:48 +0100

    Created Registerform class

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,3 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+
+
+#  Created User Registration Class
+class RegisterTraderForm(UserCreationForm):
+    class Meta:
+        model = User
+
+        fields = ("username", "password1", "password2")
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterTraderForm, self).__init__(*args, **kwargs)
+
+        self.fields["username"].widget.attrs["class"] = "form-control form-control-sm"
+        self.fields["password1"].widget.attrs["class"] = "form-control form-control-sm"
+        self.fields["password2"].widget.attrs["class"] = "form-control form-control-sm"
