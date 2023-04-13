57e719b480c04ae8ff0b7739467e3fc4fba266df
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-13 18:38:17 +0100

    Imported files for authentication

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
