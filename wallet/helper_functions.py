083d898c913736ba1369dafa566ca77b8cb2dab4
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-13 21:59:39 +0100

    Created function to get coin price from coingecko api

--- wallet/helper_functions.py
+++ wallet/helper_functions.py
@@ -3,8 +3,14 @@ from pycoingecko import CoinGeckoAPI
 import json
 
 
-# Get Coin List from CoinGeckoAPI
+#  Get Coin List from CoinGeckoAPI
 def get_coin_list():
     cg = CoinGeckoAPI()
     coin_list = list(((f"{coin['id']} ({coin['symbol'].upper()})") for coin in cg.get_coins_list()))
     return coin_list
+
+
+#  Get price
+def get_price(coin_id):
+    cg = CoinGeckoAPI()
+    return cg.get_price(ids=coin_id, vs_currencies="usd")[coin_id]["usd"]
