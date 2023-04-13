783a852f712d1e55e97e61a5d574553b0a437a54
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-13 23:00:34 +0100

    Imported utilities for views.py

--- wallet/views.py
+++ wallet/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Holding
+from .positions import *
+from .helper_functions import get_price, get_fg, txtToArray, cum_up_or_down
+from .forms import HoldingForm
+from datetime import date
+import os
 
-# Create your views here.
+
+
