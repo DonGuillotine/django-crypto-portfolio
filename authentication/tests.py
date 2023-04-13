15688c9957003b223400157096432269ebbefb58
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-13 16:27:43 +0100

    Created Forgot Password Test

--- authentication/tests.py
+++ authentication/tests.py
@@ -2,12 +2,14 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+
 # Created Login Test
 class AuthenticationTest(TestCase):
     def setUp(self):
         self.client = Client()
         self.login_url = reverse('login')
         self.user = User.objects.create_user(username='testuser', password='testpassword')
+
 
     def test_login(self):
         url = reverse('login')
@@ -24,3 +26,12 @@ class AuthenticationTest(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('login'))
+
+
+    def test_forgot_password(self):
+        url = reverse('password_reset')
+        response = self.client.post(url, {
+            'email': self.user.email
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('password_reset_done'))
