3f0e2d81565e46c547a66e12339d08e5387016dd
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-13 18:42:47 +0100

    Created login Function

--- authentication/views.py
+++ authentication/views.py
@@ -5,3 +5,23 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+
+# Function to handle Login
+def login(request):
+    if request.method == "POST":
+        #  If the request method is post get user details
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            print("success")
+            return redirect('holdings')
+        else:
+            print('failed')
+            messages.success(request, "There was an error loggin in. Please try again.")
+            return redirect('login')
+
+    else:
+        return render(request, 'authenticate/login.html', {})
