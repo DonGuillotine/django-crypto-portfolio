4f4cb147c8a4a9455db003c798b724d21093353d
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-13 21:58:51 +0100

    Created function to get coin List from coingecko api

--- wallet/helper_functions.py
+++ wallet/helper_functions.py
@@ -1,3 +1,10 @@
 import requests
 from pycoingecko import CoinGeckoAPI
 import json
+
+
+# Get Coin List from CoinGeckoAPI
+def get_coin_list():
+    cg = CoinGeckoAPI()
+    coin_list = list(((f"{coin['id']} ({coin['symbol'].upper()})") for coin in cg.get_coins_list()))
+    return coin_list
