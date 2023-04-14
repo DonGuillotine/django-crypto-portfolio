fdc72bc95b0629f83751550b95a680ceb00c2454
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-14 04:17:58 +0100

    Passed Tests for homepage

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import TestCase
+from django.urls import reverse
+from unittest.mock import patch, MagicMock
+from pycoingecko import CoinGeckoAPI
+
+class TestIndexView(TestCase):
+
+    @patch.object(CoinGeckoAPI, 'get_coins_markets')
+    def test_index_view(self, mock_get_coins_markets):
+        mock_get_coins_markets.return_value = MagicMock()
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        mock_get_coins_markets.assert_called_once_with(vs_currency='usd', order='market_cap_desc', per_page=10)
