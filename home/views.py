790d1a4824f433c8d13796f24306d5936ba2ef94
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-14 04:14:05 +0100

    Imported CoinGecko API to fetch top 10 currencies

--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+import requests
+from pycoingecko import CoinGeckoAPI
 
 # Render home page
 def index(request):
+    cg = CoinGeckoAPI()
+    top_currencies = cg.get_coins_markets(vs_currency='usd', order='market_cap_desc', per_page=10)
     return render(request, 'home.html')
