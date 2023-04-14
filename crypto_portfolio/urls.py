ce0f40396021d3da41b5a368b5025d73a297c4d8
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-14 01:01:33 +0100

    Added Urls for holdings in Main URLS

--- crypto_portfolio/urls.py
+++ crypto_portfolio/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/portfolio')),
+    path('', include('wallet.urls')),
     path('', include('home.urls')),
     path('authentication/', include('authentication.urls')),
 ]
