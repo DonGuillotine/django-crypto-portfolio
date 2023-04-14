4dcc31c2622cde2ed555f133dcbfa0d844acd307
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-14 05:02:04 +0100

    Created a Referal App Model

--- referrals/models.py
+++ referrals/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#  Referral Model with a User as a Foreign Key
+class Referral(models.Model):
+    referrer = models.ForeignKey(User, on_delete=models.CASCADE)
+    referral_code = models.CharField(max_length=30, unique=True)
+    used_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, related_name='used_referrals')
+    created_at = models.DateTimeField(auto_now_add=True)
