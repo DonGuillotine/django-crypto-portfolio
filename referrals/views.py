0983a04000da297697657bb57dd3c45679ab850a
Donald Nwokoro <infect3dlab@gmail.com>
2023-04-14 05:05:19 +0100

    Created a Referal App View

--- referrals/views.py
+++ referrals/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Referral
+from django.urls import reverse
 
-# Create your views here.
+@login_required
+def referral_link(request):
+
+    #   Generates a unique referral link for the currently logged-in user and displays it on the screen.
+    referral, created = Referral.objects.get_or_create(referrer=request.user)
+    referral_link = request.build_absolute_uri(reverse('register') + '?ref=' + referral.referral_code)
+    return render(request, 'referral/link.html', {'referral_link': referral_link})
