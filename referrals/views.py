58001379a0f1b1592f64f1f8542c52f7d9939682
Donald Nwokoro <infect3dlab@gmail.com>
2023-07-14 20:04:22 +0100

    Update views.py

--- referrals/views.py
+++ referrals/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 @login_required
 def referral_link(request):
-    #   Generates a unique referral link for the currently logged-in user and displays it on the screen.
+    # Generates a unique referral link for the currently logged-in user and displays it on the screen.
     referral, created = Referral.objects.get_or_create(referrer=request.user)
     referral_link = request.build_absolute_uri(reverse('register') + '?ref=' + referral.referral_code)
     return render(request, 'referral/link.html', {'referral_link': referral_link})
