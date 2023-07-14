18f783fd866cd4c2c69f7ca194f847823bcaea25
Donald Nwokoro <infect3dlab@gmail.com>
2023-07-14 20:04:49 +0100

    Update models.py

--- referrals/models.py
+++ referrals/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#  Referral Model with a User as a Foreign Key
+# Referral Model with a User as a Foreign Key
 class Referral(models.Model):
     referrer = models.ForeignKey(User, on_delete=models.CASCADE)
     referral_code = models.CharField(max_length=30, unique=True)
